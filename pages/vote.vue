646acdfde55c11eac6ad858c7252322dace4c3e0
王俊坤 <congti422@163.com>
2021-02-05 13:21:46 +0800

    去掉报名确认流程

--- pages/vote.vue
+++ pages/vote.vue
@@ -75,56 +75,48 @@ export default {
     },
     // 点击投票
     setVote(item) {
-      wx.showModal({
-        title: "报名确认",
-        content: "确定要报名吃 " + item.name + " 吗？",
-        success(res) {
-          if (res.confirm) {
-            wx.showLoading({ title: "报名中", mask: true });
-            votePort.isVote().then(res => {
-              if (res.data.length) {
-                wx.hideLoading();
-                if (res.data[0].voteItemId != item._id) {
-                  wx.showModal({
-                    title: "善变之人（渣男）",
-                    content: "今天已经报名了，是否修改报名结果？",
-                    confirmText: "我是渣男",
-                    success(successRes) {
-                      if (successRes.confirm) {
-                        votePort
-                          .updateVote({ _id: res.data[0]._id, id: item._id })
-                          .then(updateVoteRes => {
-                            wx.showToast({
-                              title: "报名成功",
-                              icon: "none",
-                              mask: true
-                            });
-                          });
-                      }
-                    }
-                  });
-                } else {
-                  wx.showModal({
-                    title: "别点了",
-                    content: "今天已经报名了" + item.name + "，你忘了吗？",
-                    confirmText: "我知道了",
-                    showCancel: false,
-                    success(res) {}
-                  });
+      wx.showLoading({ title: "报名中", mask: true });
+      votePort.isVote().then(res => {
+        if (res.data.length) {
+          wx.hideLoading();
+          if (res.data[0].voteItemId != item._id) {
+            wx.showModal({
+              title: "善变之人（渣男）",
+              content: "今天已经报名了，是否修改报名结果？",
+              confirmText: "我是渣男",
+              success(successRes) {
+                if (successRes.confirm) {
+                  votePort
+                    .updateVote({ _id: res.data[0]._id, id: item._id })
+                    .then(updateVoteRes => {
+                      wx.showToast({
+                        title: "报名成功",
+                        icon: "none",
+                        mask: true
+                      });
+                    });
                 }
-              } else {
-                votePort.setVote(item._id).then(res => {
-                  wx.hideLoading();
-                  wx.showToast({
-                    title: "报名成功",
-                    icon: "none",
-                    mask: true
-                  });
-                  this.getVoteList();
-                });
               }
             });
+          } else {
+            wx.showModal({
+              title: "别点了",
+              content: "今天已经报名了" + item.name + "，你忘了吗？",
+              confirmText: "我知道了",
+              showCancel: false,
+              success(res) {}
+            });
           }
+        } else {
+          votePort.setVote(item._id).then(res => {
+            wx.hideLoading();
+            wx.showToast({
+              title: "报名成功",
+              icon: "none",
+              mask: true
+            });
+            this.getVoteList();
+          });
         }
       });
     }
